f9a4e2512ac86ebc7bc97e5043a68135b9fb6493
Yeonju Oh <duswn0201@gmail.com>
2017-11-13 20:53:15 -0500

    move to one AP

--- teleop.cpp
+++ teleop.cpp
@@ -1,36 +1,54 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <signal.h>
+#include <fcntl.h>
 #include <termios.h>
 #include <stdio.h>
 #include <string>
 #include <unistd.h>
 #include <stdlib.h>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
 //my custom message
 #include <rosaria/PathName.h>
 //my custom service
 #include <rosaria/GetWayPoints.h>
 
+namespace patch
+{
+    template < typename T > std::string to_string( const T& n )
+    {
+        std::ostringstream stm ;
+        stm << n ;
+        return stm.str() ;
+    }
+}
+
 class MyP3AT {
 public:
     MyP3AT(){};
     void Loop();
-    void scanWifi();
-    ros::NodeHandle nh;
-    ros::Subscriber points;
-    rosaria::PathName msg; 
-    std::map<std::string, int> APmap;
+    void scanWifi();                            // Scan near wifi
+    void serialSetup(std::string port);         // Serial Port setup
+    
+    ros::NodeHandle nh;                         // Node handler
+    ros::Subscriber points;                     // subscriber to get waypoints from COM2 with PathName type message
+    ros::Publisher cmdvel;                      // publisher to move a robot
+    rosaria::PathName msg;                      // custom message 
+    std::map<std::string, int> requestedAPs;
+    std::map<std::string, double> nearNetworks; // store near wifi with SSID + Signal level
     double cur_posx, cur_posy;
+    int mc;                                     // variable for a serial communication with a motor controller
 };
 
-void pathWaypointsMessageReceived(const rosaria::PathName &msg, std::map<std::string, int> &APmap){
+void pathWaypointsMessageReceived(const rosaria::PathName &msg, std::map<std::string, int> &requestedAPs){
   
     for(int i=0; i<10; i++){
         std::cout<< msg.points[i] <<std::endl;
-        APmap.insert(std::pair<std::string, int>(msg.points[i], i));
+        requestedAPs.insert(std::pair<std::string, int>(msg.points[i], i));
     }
-
 //    ROS_INFO_STREAM("sibal");
 }
 
@@ -45,38 +63,171 @@ int main(int argc, char** argv){
     MyP3AT myrobot;
     myrobot = MyP3AT();
     //myrobot.points = myrobot.nh.subscribe("pathwaypoints", 1000, &pathWaypointsMessageReceived);
+    myrobot.cmdvel = myrobot.nh.advertise<geometry_msgs::Twist>("RosAria/cmd_vel", 1);
     boost::shared_ptr<rosaria::PathName const> sharedPtr;
-
+/*
     sharedPtr = ros::topic::waitForMessage<rosaria::PathName>("pathwaypoints", myrobot.nh);
+    
     if(sharedPtr == NULL){
         ROS_ERROR("Failed to get waypoints!");
         return 1;
     }
     else{
         myrobot.msg = *sharedPtr;
-        pathWaypointsMessageReceived(myrobot.msg, myrobot.APmap);
+        pathWaypointsMessageReceived(myrobot.msg, myrobot.requestedAPs);
     }
-
-    myrobot.scanWifi();
+*/
+    if(argc < 2) {
+        ROS_ERROR("%s", "Add port number as a command argument!");
+        return (1);
+    }
+    else{
+        myrobot.serialSetup(argv[1]);
+    }
     
-    fp = std::fopen("scan.txt", "rw");
-    if(fp == NULL){
-        std::cout << "File open error" <<std::endl;
-        return -1;
-    }
-
-    //myrobot.Loop();
-    std::fclose(fp);
+    ROS_INFO("%s", "Start scanning near APs...");
+    //myrobot.scanWifi();
+    ROS_INFO("%s", "Done scanning near APs...");
+    myrobot.Loop();
+    
+    close(myrobot.mc);
     ros::spin();
     return (0);
 }
 
 void MyP3AT::Loop(){
+    std::string cur_waypoint = "Boiler";
+    geometry_msgs::Twist msg;
+    msg.angular.z = 0;
+    msg.linear.x = 0;
+    //std::map<std::string, double>::iterator it = nearNetworks.find(cur_waypoint);
+    std::string line;
     
+    signed int cur_sig;
+    std::vector<double> total_sig;
+    int maxidx;
+
+    write(mc, "$A1M2ID1-085", 13);
+    system("sleep 5.0");
+    //Follow the cur waypoint until the robot arrives
+    while(1){
+        total_sig.clear();
+        maxidx = 0;
+        for(int i=0; i<180; i++){
+            std::string motorcommand = "$A1M2ID1";
+            int angle = i - 85;
+            if(angle < 0){
+                if(angle > -10) motorcommand += "-00" + patch::to_string(abs(angle));
+                else if(angle > -100) motorcommand += "-0" + patch::to_string(abs(angle));
+                else motorcommand+="-" + patch::to_string(abs(angle));
+            }
+            else{
+                if(angle<10) motorcommand += "+00" + patch::to_string(angle);
+                else if(angle<100){
+                    motorcommand += "+0" + patch::to_string(angle);
+                }
+                else{
+                    motorcommand += "+" + patch::to_string(angle);
+                }
+            }
+            
+            //std::cout<<motorcommand<<std::endl;
+            write(mc, motorcommand.c_str(), motorcommand.length());
+            system("sleep 0.01");
+
+            system("iwconfig wlx00c0ca577641 | grep Signal| cut -d - -f2 | cut -d ' ' -f 1 > sig_temp.txt");
+            std::ifstream tempfile("/home/yeonju/catkin_ws/sig_temp.txt");
+            while(!tempfile.eof()){
+                std::getline(tempfile, line, '\n');
+                std::stringstream convertor(line);
+                convertor >> cur_sig;
+                //std::cout << "["<< i <<"]"<< " " << line << " " << cur_sig <<std::endl;
+            }
+            total_sig.push_back(cur_sig);
+            if(cur_sig < total_sig[maxidx]) maxidx = i;
+        }
+        msg.linear.x = 1;
+        msg.angular.z = (maxidx-85)/100;
+        cmdvel.publish(msg);
+    }
+
+    //msg.angular.z = 0;
+    //msg.linear.x = 0;
+    //cmdvel.publish(msg);
 }
 
 void MyP3AT::scanWifi(){
-    char a;
+    char a; //system call handler
+    
+    std::string line, line2;
+    std::string ESSID;
+    double siglevel;
+    std::size_t found, end;
+    
+    //TODO: Replace your password and wireless adapter name with yours...
+    a = system("echo iuer9895 | sudo -S iwlist wlx00c0ca577641 scanning | (grep ESSID) > ssid.txt");
+    a = system("echo iuer9895 | sudo -S iwlist wlx00c0ca577641 scanning | (grep 'Signal') > signal.txt");
+    
+    //TODO: Change the location of files to your own paths.
+    std::ifstream fp("/home/yeonju/catkin_ws/ssid.txt");
+    std::ifstream fd("/home/yeonju/catkin_ws/signal.txt");
+    
+    if(fp == NULL){
+        std::cout << "[FP]File open error" <<std::endl;
+        return;
+    }
+    if(fd == NULL){
+        std::cout << "[FD]File open error" <<std::endl;
+        return;
+    }
 
-    a = system("iwlist wlp2s0 scanning | grep 'ESSID\|Signal level' > scan.txt");
+    while(std::getline(fp,line) && std::getline(fd, line2)){
+        //std::cout<<line<<std::endl;
+        
+        found = line2.find("Signal level=");
+        
+        if(found!=std::string::npos){
+            std::stringstream convertor(line2.substr(found+13,3));
+            convertor >> siglevel;
+        }
+        
+        found = line.find("ESSID:\"");
+        //std::remove(line.begin(), line.end(), ' ');
+        end = line.find("\"");
+    //std::cout<<found << " " <<end << std::endl;
+        if(found!=std::string::npos && end!=std::string::npos){
+            std::stringstream convertor2(line.substr(found+7, end-found));
+            convertor2 >> ESSID;
+        }
+        //std::cout<<ESSID <<"   "<<siglevel<<std::endl;
+        if(nearNetworks.find(ESSID) == nearNetworks.end()){ //register new AP
+            std::cout << "New near network registered: " << ESSID <<std::endl;
+             nearNetworks.insert(std::pair<std::string, double>(ESSID, siglevel));
+        }
+    }
+
+    return;
+}
+
+void MyP3AT::serialSetup(std::string port){
+    struct termios newtio;
+    std::string cmd = "stty -F " + port + " 57600";
+    
+    system(cmd.c_str());
+    mc = open(port.c_str(), O_RDWR | O_NOCTTY);
+  
+    newtio.c_cflag = B57600;
+    newtio.c_cflag |= CS8;
+    newtio.c_cflag |= CLOCAL;
+    newtio.c_cflag |= CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+    newtio.c_lflag = 0;
+    newtio.c_cc[VTIME] = 0;
+    newtio.c_cc[VMIN] = 1;
+  
+    tcflush (mc, TCIFLUSH);
+    tcsetattr(mc, TCSANOW, &newtio);
+
+    ROS_INFO("%s", "Serial Port is opend!");
 }
--- testpub.cpp
+++ testpub.cpp
@@ -6,98 +6,47 @@
 #include <vector>
 #include <string>
 
-#include "serial/serial.h"
 #include <unistd.h>
+#include <fcntl.h>
 
+#include <termios.h>
 
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
+
   ros::init(argc, argv, "talker");
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
   ros::NodeHandle n;
 
-  /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
-   */
   ros::Publisher chatter_pub = n.advertise<rosaria::PathName>("pathwaypoints", 1000);
+  int fd;
+  struct termios newtio;
 
-  if(argc < 2){
-    std::cout<<"argc!"<<std::endl;
-    return 0;
-  } 
-  std::string port(argv[1]);
-  if(port == "-e"){
-    //enumerate_ports();
-    return 0;
+  system("stty -F /dev/ttyUSB0 57600");
+  fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY);
 
-  }
-  else if(argc < 3){
-    std::cout <<"argc2!"<<std::endl;
-    return 1;
-  }
+  newtio.c_cflag = B57600;
+  newtio.c_cflag |= CS8;
+  newtio.c_cflag |= CLOCAL;
+  newtio.c_cflag |= CREAD;
+  newtio.c_iflag = IGNPAR;
+  newtio.c_oflag = 0;
+  newtio.c_lflag = 0;
+  newtio.c_cc[VTIME] = 0;
+  newtio.c_cc[VMIN] = 1;
 
-  unsigned long baud = 0;
-  #if defined(WIN32) && !defined(__MINGW32__)
-    std::sscanf_s(argv[2], "%lu", &baud);
-  #else
-    std::sscanf(argv[2], "%lu", &baud);
-  #endif
+  tcflush (fd, TCIFLUSH);
+  tcsetattr(fd, TCSANOW, &newtio);
 
-  // port, baudrate, timeout in milliseconds
-  serial::Serial my_serial(port, baud, serial::Timeout::simpleTimeout(1000));
-  
-    std::cout << "Is the serial port open?";
-    if(my_serial.isOpen())
-      std::cout << " Yes." << std::endl;
-    else
-      std::cout << " No." << std::endl;
-  
-  
-  ros::Rate loop_rate(10);
-  
+  ros::Rate loop_rate(1000);
+  write(fd, "#", 1);
 
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
   int count = 0;
   while (ros::ok())
   {
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
+    unsigned char bytes;
+    unsigned char buf[1];
+
     rosaria::PathName msg;
     std::vector<std::string> test;
     
@@ -109,24 +58,24 @@ int main(int argc, char **argv)
     }
     msg.points = test;
 
-  //  std::write()
-    //strcpy(temp, msg.points);
-    std::cout<<msg<<std::endl;
-
-    /**
-     * The publish() function is how you send messages. The parameter
-     * is the message object. The type of this object must agree with the type
-     * given as a template parameter to the advertise<>() call, as was done
-     * in the constructor above.
-     */
     chatter_pub.publish(msg);
-    my_serial.write("$A2M3ID1+00400");
+    
+    write(fd, "$A1M2ID1-0085", 13);
+    bytes = read(fd, buf, sizeof(buf));
+    std::string data = "";
+    for(int j=0; j<bytes; j++){
+      data.append(1, buf[j]);
+      
+    }
+    ROS_INFO("%s", data);
+    //my_serial.write("$A1M3ID1+0400");
+    //my_serial.write("$A3M3ID1+0400");
     ros::spinOnce();
 
     loop_rate.sleep();
     ++count;
   }
 
-  
+  close(fd);
   return 0;
 }
