941745e3ab6661304c3fb32cdc301ae07047dfc4
Yeonju Oh <oh132@purdue.edu>
2018-01-09 00:42:35 -0500

    edited rss measure test code

--- src/rssimeasuretest.cpp
+++ src/rssimeasuretest.cpp
@@ -1,4 +1,6 @@
 #include <rosaria/teleop.h>
+#include <atomic>
+#include <thread>
 using namespace std;
 
 static const string WADAPTER = "wlan0";
@@ -34,35 +36,52 @@ void SerialSetup(char* port){
     write(mc, "$A1M3ID1-400", 13);
 }
 
-void RSSI(){
+void RSSI(atomic< bool >& run){
     int cur_sig;
     string cmd = "iwconfig " + WADAPTER + " | grep Signal| cut -d - -f2 | cut -d ' ' -f 1 > sig_temp.txt";
-
-    system(cmd.c_str());
-
-    ifstream tempfile("/home/yeonju/catkin_ws/sig_temp.txt");
-    while(!tempfile.eof()){
-        std::getline(tempfile, line, '\n');
-        std::stringstream convertor(line);
-        convertor >> cur_sig;
-        cout << cur_sig << endl;
+    cout << "Thread RSSI has started" <<endl;
+    while(run){
+        system(cmd.c_str());
+        this_thread::sleep_for(0.01s);
+        ifstream tempfile("/home/yeonju/catkin_ws/sig_temp.txt");
+        while(!tempfile.eof()){
+            std::getline(tempfile, line, '\n');
+            std::stringstream convertor(line);
+            convertor >> cur_sig;
+            cout << cur_sig << endl;
+        }
     }
+
+    cout << "Thread RSSI has terminated" <<endl;
 }
 
 int main(int argc, char** argv){
     ros::init(argc, argv, "rssimeasure");
 
+    atomic < bool > run{ true };
+    thread th{ RSSI, ref(run) };
+    run = false;
+    
     Init(argv[1]);
     int count = 0;
     
 
     while(1){
-        write(mc, "$A1M2ID1-090", 13);
+        write(mc, "$A1M2ID1+090", 13);
         cout<< "--------------------- [" << count << "] -----------------------" <<endl;
+        run = true;
         
         cout<< "---------------------------------------------------------------" <<endl;
         system("sleep 1.0");
+        //Or use
+        //this_thread::sleep_for(1.0s);
+        run = false;
         write(mc, "$A1M2ID1-090", 13);
         system("sleep 1.0");
+        //this_thread::sleep_for(1.0s);
     }
+    run = false;
+    th.join();
+
+    return 0;
 }
